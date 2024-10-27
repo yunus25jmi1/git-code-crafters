7fc0ac143ca8d58d1fb3cbc7b3e09931bbd40a5b
Md Yunus <admin@yunuscollege.eu.org>
2024-10-27 22:23:30 +0530

    [any message]

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -18,19 +18,19 @@ func main() {
 	switch command := os.Args[1]; command {
 	case "init":
 		// Uncomment this block to pass the first stage!
-		//
-		// for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-		// 	if err := os.MkdirAll(dir, 0755); err != nil {
-		// 		fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-		// 	}
-		// }
-		//
-		// headFileContents := []byte("ref: refs/heads/main\n")
-		// if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-		// 	fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
-		// }
-		//
-		// fmt.Println("Initialized git directory")
+		
+		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+			}
+		}
+		
+		headFileContents := []byte("ref: refs/heads/main\n")
+		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+		}
+		
+		fmt.Println("Initialized git directory")
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
